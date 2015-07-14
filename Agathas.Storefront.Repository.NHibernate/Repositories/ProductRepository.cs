bed2096bd7df33a50e29d8ebb28c4a625197af73
afrancocode <afranco@codeservices.com.mx>
2015-07-14 16:23:46 -0700

    Repository Support and Model Objects
    Basic Repository Support with some Model Objects

---
+++ Agathas.Storefront.Repository.NHibernate/Repositories/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+
+using Agathas.Storefront.Infrastructure.UnitOfWork;
+using Agathas.Storefront.Model.Products;
+
+namespace Agathas.Storefront.Repository.NHibernate.Repositories
+{
+	public class ProductRepository : Repository<Product, int>, IProductRepository
+	{
+		public ProductRepository(IUnitOfWork uow)
+			: base(uow)
+		{ }
+
+		public override void AppendCriteria(ICriteria criteria)
+		{
+			criteria.CreateAlias("Title", "ProductTitle");
+			criteria.CreateAlias("ProductTitle.Category", "Category");
+			criteria.CreateAlias("ProductTitle.Brand", "Brand");
+			criteria.CreateAlias("ProductTitle.Color", "Color");
+		}
+	}
+}
