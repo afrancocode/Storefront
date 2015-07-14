35e3a2575af5be67cab5d044846efbaef396b887
afrancocode <afranco@codeservices.com.mx>
2015-07-13 18:40:47 -0700

    Infrastructure
    Adding basic infrastructure code

---
+++ Agathas.Storefront.Infrastructure/Domain/ValueObjectIsInvalidException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agathas.Storefront.Infrastructure.Domain
+{
+	public class ValueObjectIsInvalidException : Exception
+	{
+		public ValueObjectIsInvalidException(string message)
+			: base(message)
+		{
+		}
+	}
+}
