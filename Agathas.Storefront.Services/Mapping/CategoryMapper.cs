a7639ffb06732582131620244ed0e6e7dd5e2c76
afrancocode <afranco@codeservices.com.mx>
2015-07-14 17:30:03 -0700

    Services
    Base Service Changes

---
+++ Agathas.Storefront.Services/Mapping/CategoryMapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+using Agathas.Storefront.Model.Categories;
+using Agathas.Storefront.Services.ViewModels;
+
+namespace Agathas.Storefront.Services.Mapping
+{
+	public static class CategoryMapper
+	{
+		public static IEnumerable<CategoryView> ConvertToCategoryViews(this IEnumerable<Category> categories)
+		{
+			return Mapper.Map<IEnumerable<Category>, IEnumerable<CategoryView>>(categories);
+		}
+	}
+}
---
+++ Agathas.Storefront.Services/Mapping/IProductAttributeMapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agathas.Storefront.Model.Products;
+using Agathas.Storefront.Services.ViewModels;
+using AutoMapper;
+
+namespace Agathas.Storefront.Services.Mapping
+{
+	public static class IProductAttributeMapper
+	{
+		public static RefinementGroup ConvertToRefinementGroup(
+			this IEnumerable<IProductAttribute> productAttributes,
+			RefinementGroupings refinementGroupType)
+		{
+			RefinementGroup refinementGroup = new RefinementGroup()
+			{
+				Name = refinementGroupType.ToString(),
+				GroupId = (int)refinementGroupType
+			};
+
+			refinementGroup.Refinements = 
+				Mapper.Map<IEnumerable<IProductAttribute>, IEnumerable<Refinement>>(productAttributes);
+
+			return refinementGroup;
+		}
+	}
+}
