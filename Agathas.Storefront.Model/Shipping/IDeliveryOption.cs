5810d8831ee9730acbc08015c27d9fc87897890e
afrancocode <afranco@codeservices.com.mx>
2015-07-22 09:09:54 -0700

    Basket Model
    Basket Model Support

---
+++ Agathas.Storefront.Model/Shipping/IDeliveryOption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agathas.Storefront.Model.Shipping
+{
+	public interface IDeliveryOption
+	{
+		int Id { get; set; }
+		decimal FreeDeliveryThreshold { get; }
+		decimal Cost { get; }
+		ShippingService ShippingService { get; }
+		decimal GetDeliveryChargeForBasketTotalOf(decimal total);
+	}
+}
