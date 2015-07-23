9f15913ced7c0bd6a5d2bced403659d141d5e469
afrancocode <afranco@codeservices.com.mx>
2015-07-22 17:53:05 -0700

    Customer and Account Controllers

---
+++ Agathas.Storefront.Controllers/ViewModels/CustomerAccount/BaseCustomerAccountView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agathas.Storefront.Controllers.ViewModels.CustomerAccount
+{
+	public abstract class BaseCustomerAccountView
+	{
+		public BasketSummaryView BasketSummary { get; set; }
+	}
+}
---
+++ Agathas.Storefront.Controllers/ViewModels/CustomerAccount/CustomerDetailView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Agathas.Storefront.Services.ViewModels;
+
+namespace Agathas.Storefront.Controllers.ViewModels.CustomerAccount
+{
+	public class CustomerDetailView : BaseCustomerAccountView
+	{
+		public CustomerView Customer { get; set; }
+	}
+}
