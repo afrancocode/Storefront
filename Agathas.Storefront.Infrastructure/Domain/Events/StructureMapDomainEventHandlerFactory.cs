9e5bdbed7b9c2e65800ecf42c0f5e15e1ebee173
afrancocode <afranco@codeservices.com.mx>
2015-07-22 18:17:07 -0700

    Domain Events
    Domain Events Framework

---
+++ Agathas.Storefront.Infrastructure/Domain/Events/StructureMapDomainEventHandlerFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StructureMap;
+
+namespace Agathas.Storefront.Infrastructure.Domain.Events
+{
+	public class StructureMapDomainEventHandlerFactory : IDomainEventHandlerFactory
+	{
+		public IEnumerable<IDomainEventHandler<T>> GetDomainEventHandlersFor<T>(T domainEvent) where T : IDomainEvent
+		{
+			return ObjectFactory.GetAllInstances<IDomainEventHandler<T>>();
+		}
+	}
+}
